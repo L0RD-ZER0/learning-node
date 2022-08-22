3755663f5dd7ebbdad5e020105bb0a3ceeaec856
Zero <ackerhon@gmail.com>
2022-08-22 14:42:56 +0530

    Lesson-33

--- basic-node-server/app.js
+++ basic-node-server/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require("fs");
 
 
 // function requestListener(req, res) {
@@ -13,12 +14,19 @@ const server = http.createServer((req, res) => {
     // process.exit();
 
     const url = req.url;
+    const method = req.method;
+
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html lang="en">');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="post"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.writeHead(302, {'Location': '/'});
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
