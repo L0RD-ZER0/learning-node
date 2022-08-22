5168413f1e44950077299c2874df08c91f072c35
Zero <ackerhon@gmail.com>
2022-08-22 14:28:02 +0530

    Lesson-30

--- basic-node-server/app.js
+++ basic-node-server/app.js
@@ -11,6 +11,12 @@ const server = http.createServer((req, res) => {
     // console.log(req, res);
     console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html lang="en">');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from Node.JS!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(process.env.PORT || 3000);
