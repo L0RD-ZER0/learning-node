028b6c5a90a9454264ffaf49c9d549240ec3f9ca
Zero <ackerhon@gmail.com>
2022-08-25 14:17:51 +0530

    Lesson-63

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -3,12 +3,17 @@ import express, { Express } from 'express';
 const PORT = process.env.PORT || '3000';
 const app: Express = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next();  // allow request to continue onwards
+// app.use((req, res, next) => {
+//   console.log('In the middleware');
+//   next();  // allow request to continue onwards
+// });
+
+app.use('/add-product', (req, res) => {  // matching is done on the basis of starts-with
+  console.log('In another middleware');
+  res.send('<h1>The "App-Product" page.</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res) => {  // matching is done on the basis of starts-with
   console.log('In another middleware');
   res.send('<h1>Hello from Express!</h1>');
 });
