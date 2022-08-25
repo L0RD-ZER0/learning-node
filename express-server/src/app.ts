2062129f7587a3652c70aeb543e3c01c1b582bc3
Zero <ackerhon@gmail.com>
2022-08-25 14:04:11 +0530

    Lesson-60

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -3,5 +3,13 @@ import express, { Express } from 'express';
 const PORT = process.env.PORT || '3000';
 const app: Express = express();
 
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next();  // allow request to continue onwards
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+});
 
 app.listen(PORT, () => console.log(`Server started at localhost:${ PORT }`));
