42ba98b6a246062a3d4332878d8da82ab2680693
Zero <ackerhon@gmail.com>
2022-08-25 15:17:46 +0530

    Lesson-64

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import bodyParser from 'body-parser';
 
 const PORT = process.env.PORT || '3000';
 const app: Express = express();
@@ -8,13 +9,20 @@ const app: Express = express();
 //   next();  // allow request to continue onwards
 // });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res) => {  // matching is done on the basis of starts-with
-  console.log('In another middleware');
-  res.send('<h1>The "App-Product" page.</h1>');
+  // console.log('In another middleware');
+  res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res) => {  // matching is done on the basis of starts-with
-  console.log('In another middleware');
+  // console.log('In another middleware');
   res.send('<h1>Hello from Express!</h1>');
 });
 
