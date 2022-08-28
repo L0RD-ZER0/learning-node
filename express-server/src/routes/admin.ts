94180ff06590c287be5777bd20090ead640c99d2
Zero <ackerhon@gmail.com>
2022-08-29 00:37:29 +0530

    Lesson-79

--- express-server/src/routes/admin.ts
+++ express-server/src/routes/admin.ts
@@ -4,6 +4,8 @@ import { makePath } from "../utils";
 
 export const router = Router();
 
+export const products: string[] = [];
+
 router.get('/add-product', (req, res) => {  // matching is done on the basis of starts-with
   // console.log('In another middleware');
   // res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
@@ -12,6 +14,7 @@ router.get('/add-product', (req, res) => {  // matching is done on the basis of 
 
 router.post('/add-product', (req, res) => {
   console.log(req.body);
+  products.push(req.body.title);
   res.redirect('/');
 });
 
--- express-server/src/routes/shop.ts
+++ express-server/src/routes/shop.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { makePath } from "../utils";
 
+import { products } from "./admin";
+
 
 export const router = Router();
 
 router.get('/', (req, res) => {  // matching is done on the basis of starts-with
   // console.log('In another middleware');
   // res.send('<h1>Hello from Express!</h1>');
+  console.log('shop.js:', products);
   res.sendFile(makePath('src', 'views', 'shop.html'));
 });
 
